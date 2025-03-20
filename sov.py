544fbc9c2493d1fa447100981182a431681c12f4
Seovnix <119292577+Seovnix@users.noreply.github.com>
2025-03-20 18:31:22 +0100

    Update sov.py

--- sov.py
+++ sov.py
@@ -23,7 +23,7 @@ def obtenir_reponse(question):
     return completion.choices[0].message.content
 
 def generer_questions(marque):
-    prompt_questions = f"""Génère 5 questions associées à la marque : {marque} en fonction de ce que les internautes cherchent le plus sur Google"""
+    prompt_questions = f"""Trouve le secteur de cette marque et identifie les tops questions que les internautes peuvent se poser en 2025 sur la thématique de ses produits ou services: {marque}. Fais en fonction de ce que les internautes cherchent le plus sur Google"""
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{"role": "user", "content": prompt_questions}]
