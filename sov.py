d913c0f35e9244eee93f896b2bab2b7a455e6264
Seovnix <119292577+Seovnix@users.noreply.github.com>
2025-03-20 18:28:57 +0100

    Update sov.py

--- sov.py
+++ sov.py
@@ -18,7 +18,7 @@ def obtenir_reponse(question):
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{"role": "user", "content": question}],
-        temperature=0.2
+        temperature=1
     )
     return completion.choices[0].message.content
 
